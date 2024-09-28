e99701e2a3967cf042e1ec5046d614b70ec2debd
collinsrobert <52458276+collinsrobert@users.noreply.github.com>
2024-09-28 18:39:15 -0500

    Update Process_SSRS_RDL_Information.sql

--- Process_SSRS_RDL_Information.sql
+++ Process_SSRS_RDL_Information.sql
@@ -1,10 +1,17 @@
 /***
-Process SSRS RDL information for easy retrieval
 
+Author: Collins Robert
+Date:
+Process SSRS RDL information for easy retrieval
+This will provide the following information about SSRS reports
+You need to ensure this is being processed from the database server hosting the reports.
+This assusmes that the database name is ReportServer
 
 ****/
 
------Create Tables below
+-----####################################################Create Tables below
+-----#####################rdl_info_processed
+-----#####################
 USE [Master]
 GO
 
@@ -22,5 +29,28 @@ CREATE TABLE [dbo].[rdl_info_processed](
 	[DataSourceName] [varchar](max) NULL,
 	[CommandType] [varchar](20) NULL,
 	[CommandText] [varchar](max) NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+
+USE [IMG_DataServices]
+GO
+
+/****** Object:  Table [dbo].[rdl_info_master]    Script Date: 9/28/2024 6:39:02 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[rdl_info_master](
+	[Name] [nvarchar](425) NOT NULL,
+	[Type] [int] NOT NULL,
+	[Path] [varchar](200) NOT NULL,
+	[TypeDescription] [varchar](14) NOT NULL,
+	[Report Description] [varchar](500) NULL,
+	[ContentXML] [xml] NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
