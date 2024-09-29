6f0091eb4782ab2c941a70574e1642a4a82f086b
Collins Robert <collins.o.robert@gmail.com>
2024-09-29 13:12:26 -0500

    Create SSRS_Report_Subscription_Owner_update.sql

---
+++ SSRS_Report_Subscription_Owner_update.sql
@@ -0,0 +1,13 @@
+
+
+-------##########################################Procedure
+/*
+*Author: Collins Robert
+*Date:4/03/2019
+*Date modified:7/19/2023     BY: Collins Robert
+*Date modified:      BY:  
+*Description:################################################################################
+                      #This procedure monitors report subscription owner.####################
+                      #It alerts data Services when the owner is not svc_ACCOUNT#########
+                      ####################################################################### 
+*/
