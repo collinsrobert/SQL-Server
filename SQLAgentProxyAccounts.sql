5221924fad51a63ece113dc47d8c583cb5bd9d3c
Collins Robert <collins.o.robert@gmail.com>
2024-09-30 10:28:56 -0500

    Update SQLAgentProxyAccounts.sql

--- SQLAgentProxyAccounts.sql
+++ SQLAgentProxyAccounts.sql
@@ -86,4 +86,58 @@ EXEC msdb.dbo.sp_grant_proxy_to_subsystem @proxy_name=N'PowerShell_Proxy', @subs
 GO
 
 --------------Once you create the proxy account, you can then use it when defining a SQL Agent Job Step.
+-------Below is a sample job executing SSAS cube and running as SSAS_Proxy proxy account sample we created above
+
+
+USE [msdb]
+GO
+DECLARE @jobId BINARY(16)
+EXEC  msdb.dbo.sp_add_job @job_name=N'SSAS_Cube_Processing_Job', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+select @jobId
+GO
+EXEC msdb.dbo.sp_add_jobserver @job_name=N'SSAS_Cube_Processing_Job', @server_name = N'SPV-SDW01'
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_add_jobstep @job_name=N'SSAS_Cube_Processing_Job', @step_name=N'Process cube', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_fail_action=2, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'ANALYSISCOMMAND', 
+		@command=N'<Process xmlns="http://schemas.microsoft.com/analysisservices/2003/engine">
+  <Type>ProcessFull</Type>
+  <Object>
+    <DatabaseID>90Day_Sales</DatabaseID>
+  </Object>
+</Process>', 
+		@server=N'ssas_servername', ---------------------insert the correct server name here
+		@database_name=N'master', 
+		@flags=0, 
+		@proxy_name=N'SSAS_Proxy'---------------------------specify the job step to be executed using the SSAS_Proxy proxy account. This account should have the privilege need on the remote server to execute the step
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_update_job @job_name=N'SSAS_Cube_Processing_Job', 
+		@enabled=1, 
+		@start_step_id=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@description=N'', 
+		@category_name=N'[SSAS Category]', 
+		@owner_login_name=N'sa', 
+		@notify_email_operator_name=N'', 
+		@notify_page_operator_name=N''
+GO
 
