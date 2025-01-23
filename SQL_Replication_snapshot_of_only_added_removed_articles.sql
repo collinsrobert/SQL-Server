409e40dabc4a7c76b060229844fc40c5b6d0a171
Collins Robert <collins.o.robert@gmail.com>
2025-01-23 13:10:55 -0600

    Update SQL_Replication_snapshot_of_only_added_removed_articles.sql

--- SQL_Replication_snapshot_of_only_added_removed_articles.sql
+++ SQL_Replication_snapshot_of_only_added_removed_articles.sql
@@ -1,34 +1,54 @@
-use ODS
-go
-EXEC sp_changepublication
-@publication ='Publication_Name',
-@property='immediate_sync',
-@value='false'
+
+ALso get change tracking
+Permissoins
+Indexes scripted out.
+
+
+Run the following on the publication server
+
+
+select @@SERVERNAME  ----returns the servername
+
+use Database_Name
 go
 
 EXEC sp_changepublication
-@publication ='Publication_Name',
+@publication ='Publication_Name',---Add the publication Name
 @property='allow_anonymous',
 @value='false'
 go
 
-select @@SERVERNAME
+EXEC sp_changepublication
+@publication ='Publication_Name',  ---Add the publication Name
+@property='immediate_sync',
+@value='false'
+go
+
+--If there is an error above, re-run the queries above
 
 --make changes
+This involves either adding a new article to the publication or removing, modifying and adding back an exisiting article to the publication.
+Remove the article from the publication
+Modify the article outside the publication
+Add the article to the publication
+
+
 ---run snapshot
 
-
-
-use ODS
+-- Execute the scripts below.
+use Database_Name
 go
 EXEC sp_changepublication
-@publication ='Publication_Name',
+@publication ='Publication_Name',---Add the publication Name
+@property='allow_anonymous',
+@value='true'
+go
+
+EXEC sp_changepublication
+@publication ='Publication_Name',---Add the publication Name
 @property='immediate_sync',
 @value='true'
 go
 
-EXEC sp_changepublication
-@publication ='Publication_Name',
-@property='allow_anonymous',
-@value='true'
-go
+
+--If there is an error above, re-run the queries above
