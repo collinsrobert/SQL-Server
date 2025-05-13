152356c85d14c68d38fe81f0dd83e8db50e463b5
Collins Robert <collins.o.robert@gmail.com>
2025-05-13 18:30:25 -0500

    Update MemoryManager.sql

--- MemoryManager.sql
+++ MemoryManager.sql
@@ -4,5 +4,5 @@
                 ,       convert(varchar(10),cntr_value) as cntr_value
                 from sys.dm_os_performance_counters
                 where ( 
-				(object_name like '%Manager%') and (counter_name = 'Memory Grants Pending' or counter_name='Memory Grants Outstanding' ))
+				(object_name like '%Manager%') and (counter_name = 'Memory Grants Pending' or counter_name='Memory Grants Outstanding' or counter_name = 'Page life expectancy' /*or counter_name = 'Stolen pages'*/ ))
             
