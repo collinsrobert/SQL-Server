9bef0ebe9ffa89d69ac0092777c112caa2054602
Collins Robert <collins.o.robert@gmail.com>
2025-05-13 17:23:37 -0500

    Update performance_tuning.sql

--- perfromance_tuning.sql
+++ perfromance_tuning.sql
@@ -834,3 +834,32 @@ exec sp_executesql @stmt=N'SELECT
           (select cpu_count from sys.dm_os_sys_info) as number_of_cpus,
           (select scheduler_count from sys.dm_os_sys_info) as number_of_schedulers,
           (select COUNT(*) from sys.dm_xe_sessions) as number_of_xevent_sessions 
+
+
+----Buffer cache hit queries ####################################################################################################################
+
+ 
+          select top 20
+          r.session_id,
+          r.request_id,
+          --master.dbo.fn_varbintohexstr(sql_handle) as sql_handle,
+          --master.dbo.fn_varbintohexstr(plan_handle) as plan_handle,
+          case when LEN(qt.text) < 2048 then qt.text else LEFT(qt.text, 2048) + N'...' end as query_text,
+		r.statement_start_offset, 
+		r.statement_end_offset, 
+		r.logical_reads,
+		r.reads,
+		r.writes,
+		r.wait_type, 
+		r.wait_time, 
+		r.wait_resource,
+		r.blocking_session_id,
+		case when r.logical_reads > 0 then (r.logical_reads - isnull(r.reads, 0)) / convert(float, r.logical_reads)
+			else NULL
+			end as cache_hit_ratio
+	from sys.dm_exec_requests r
+		join sys.dm_exec_sessions s on r.session_id = s.session_id
+		outer apply sys.dm_exec_sql_text(r.sql_handle) as qt
+	where s.is_user_process = 0x1 and (r.reads > 0 or r.writes > 0)
+	order by (r.reads + r.writes) desc
+    
