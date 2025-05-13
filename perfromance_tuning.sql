eb722d002f663b57fd9507ddf7af274573ab9f4f
Collins Robert <collins.o.robert@gmail.com>
2025-05-13 17:17:34 -0500

    Update perfromance_tuning.sql

--- perfromance_tuning.sql
+++ perfromance_tuning.sql
@@ -822,3 +822,15 @@ exec sp_executesql @stmt=N'SELECT
           from sys.dm_io_virtual_file_stats(NULL, NULL) fs
           join sys.master_files m on fs.database_id = m.database_id and fs.file_id = m.file_id
        
+----Miscellaneous Information  ###################################################################################
+
+ 
+          select
+          (select count(*) from sys.traces) as running_traces,
+          (select count(*) from sys.databases) as number_of_databases,
+          (select count(*) from sys.dm_db_missing_index_group_stats) as missing_index_count,
+          (select waiting_tasks_count from sys.dm_os_wait_stats where wait_type = N'SQLCLR_QUANTUM_PUNISHMENT') as clr_quantum_waits,
+          (select count(*) from sys.dm_os_ring_buffers where ring_buffer_type = N'RING_BUFFER_SCHEDULER_MONITOR' and record like N'%NonYieldSchedBegin%') as non_yield_count,
+          (select cpu_count from sys.dm_os_sys_info) as number_of_cpus,
+          (select scheduler_count from sys.dm_os_sys_info) as number_of_schedulers,
+          (select COUNT(*) from sys.dm_xe_sessions) as number_of_xevent_sessions 
