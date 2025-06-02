df5e50535e9e60a3d192e891dff657c13b9615cc
Collins Robert <collins.o.robert@gmail.com>
2025-06-02 10:52:51 -0500

    Update enable_machine_learning.sql

--- enable_machine_learning.sql
+++ enable_machine_learning.sql
@@ -1,4 +1,16 @@
+
+
+
 --The Machine Learning Services feature can be enabled by running the following command:
 
 EXEC sp_configure 'external scripts enabled', 1;
 RECONFIGURE WITH OVERRIDE;
+
+
+
+---error when enabling machine learning below
+
+--Msg 39020, Level 16, State 1, Procedure sp_configure, Line 177 [Batch Start Line 0]
+--Feature 'Advanced Analytics Extensions' is not installed. Please consult Books Online for more information on this feature.
+
+--#########################################SOLUTION
