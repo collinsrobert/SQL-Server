d35f6d55ad31034aa17274e5891c4b5f706f0f65
Collins Robert <collins.o.robert@gmail.com>
2025-08-04 11:52:50 -0400

    Update rebuild_all_disabled_indexes.sql

--- rebuild_all_disabled_indexes.sql
+++ rebuild_all_disabled_indexes.sql
@@ -1,18 +1,16 @@
--- replace SCHEMA_NAME with your schema
-
 SELECT 
     t.name AS TableName,
+	schema_name(t.schema_id) schemaname,
     i.name AS IndexName,
 	i.is_disabled,
 	i.type_desc,
-	'alter index ['+i.name+'] ON SCHEMA_NAME.'+t.name +' rebuild'
+	'alter index ['+i.name+'] ON '+schema_name(t.schema_id)+'.'+t.name +' rebuild'
 FROM 
     sys.indexes i
 JOIN 
     sys.tables t ON i.object_id = t.object_id
 WHERE 
-    i.type_desc = 'NONCLUSTERED' OR
-	i.type_desc = 'CLUSTERED'
+    i.type_desc in( 'NONCLUSTERED','CLUSTERED')
     AND i.name IS NOT NULL and i.is_disabled<>0
 
 
